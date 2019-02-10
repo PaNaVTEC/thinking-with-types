216321d5168ca477ef9dee275ee12fe2efc36edf
Christian Panadero <panavtec@gmail.com>
2019-02-10 17:46:16 +0000

    cont monad transformer

--- src/ContinuationMonad.hs
+++ src/ContinuationMonad.hs
@@ -29,3 +29,52 @@ idCPS a ret = ret a
 
 sum1CPS :: Int -> (Int -> Int) -> Int
 sum1CPS a f = f a
+
+withVersionNumber :: (Double -> r) -> r
+withVersionNumber f = f 1.0
+
+withTimestamp :: (Int -> r) -> r
+withTimestamp f = f 1532083362
+
+withOS :: (String -> r) -> r
+withOS f = f "linux"
+
+runCont :: (forall r. (a -> r) -> r) -> a
+runCont f =
+  let callback = id
+  in f callback
+
+-- Difference of usage
+
+releaseString :: String
+releaseString =
+  withVersionNumber $ \version ->
+    withTimestamp $ \date ->
+      withOS $ \os ->
+        os <> "-" <> show version <> "-" <> show date
+
+releaseStringCont :: String
+releaseStringCont = runCont $ unCont $ do
+    version <- Cont withVersionNumber
+    date    <- Cont withTimestamp
+    os      <- Cont withOS
+    pure $ os ++ "-" ++ show version ++ "-" ++ show date
+
+newtype ContT m a = ContT { runContT :: forall r. (a -> m r) -> m r }
+
+instance Functor (ContT m) where
+  fmap :: (a -> b) -> ContT m a -> ContT m b
+  fmap ab (ContT arr) = ContT $ \ar -> arr (ar . ab)
+
+instance Applicative (ContT m) where
+  pure :: a -> ContT m a
+  pure a = ContT $ \ar -> ar a
+
+  (<*>) :: ContT m (a -> b) -> ContT m a -> ContT m b
+  (<*>) (ContT abr) (ContT ar) = ContT $ \a -> abr $ \b -> ar (a . b)
+
+instance Monad (ContT m) where
+  return = pure
+
+  (>>=) :: ContT m a -> (a -> ContT m b) -> ContT m b
+  (>>=) ca abrr = join $ pure abrr <*> ca
