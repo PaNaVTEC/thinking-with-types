985a459cae9f34aed00d972cd2dfe9ab9c15f487
Christian Panadero <panavtec@gmail.com>
2019-03-02 16:04:14 +0000

    open sums

--- src/OpenSums.hs
+++ src/OpenSums.hs
@@ -20,6 +20,8 @@ import Fcf
 import GHC.TypeLits   hiding (type (+))
 import Unsafe.Coerce
 
+import Data.Functor.Identity
+
 data OpenSum (f :: k -> Type) (ts :: [k]) where
   -- t is existential
   UnsafeOpenSum :: Int -> f t -> OpenSum f ts
@@ -37,16 +39,50 @@ findElem = fromIntegral
 inj :: forall f t ts. Member t ts => f t -> OpenSum f ts
 inj = UnsafeOpenSum (findElem @t @ts)
 
+a :: OpenSum Identity '[String]
+a = inj (Identity "1")
+
 prj :: forall f t ts. Member t ts => OpenSum f ts -> Maybe (f t)
-prj (UnsafeOpenSum i f) =
+prj (UnsafeOpenSum i f') =
   if i == findElem @t @ts
-  then Just $ unsafeCoerce f
+  then Just $ unsafeCoerce f'
   else Nothing
 
-decompose :: OpenSum f (t ': ts) -> Either (f t) (OpenSum f ts)
-decompose (UnsafeOpenSum 0 t) = Left  $ unsafeCoerce t
-decompose (UnsafeOpenSum n t) = Right $ UnsafeOpenSum (n - 1) t
+b :: Maybe (Identity String)
+b = prj a
 
--- TODO
+decompose :: OpenSum f (t ': ts) -> Either (f t) (OpenSum f ts)
+decompose (UnsafeOpenSum 0 ft) = Left  $ unsafeCoerce ft
+decompose (UnsafeOpenSum n ft) = Right $ UnsafeOpenSum (n - 1) ft
+
+c :: Either (Identity String) (OpenSum Identity '[])
+c = decompose a
+
 weaken :: OpenSum f ts -> OpenSum f (x ': ts)
-weaken _ = undefined
+weaken (UnsafeOpenSum n t) = UnsafeOpenSum (n + 1) t
+
+d :: OpenSum Identity '[Int, String]
+d = weaken a
+
+e :: Either (Identity Int) (OpenSum Identity '[String])
+e = decompose d
+
+f :: Either (Identity String) (OpenSum Identity '[])
+f = do
+      eiIS <- decompose foo
+      decompose eiIS
+  where
+    foo :: OpenSum Identity '[String, String]
+    foo = weaken (inj (Identity "1"))
+
+match :: forall f ts b
+  . (forall t. f t -> b)
+  -> OpenSum f ts
+  -> b
+match fn (UnsafeOpenSum _ t) = fn t
+
+g :: String
+g = match aFn d
+  where
+    aFn (Identity i) = unsafeCoerce i
+--    aFn (Identity (s :: String)) = s
